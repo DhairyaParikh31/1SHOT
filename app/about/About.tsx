2822f3f39a9c2e240f99dabd37e0f45005e1366a
DhairyaParikh31 <dhairyaparikh31@gmail.com>
2024-12-09 15:25:52 +0530

    commit

--- app/about/About.tsx
+++ app/about/About.tsx
@@ -12,12 +12,12 @@ const About = () => {
       <br />
       <div className="w-[95%] 800px:w-[85%] m-auto">
         <p className="text-[18px] font-Poppins">
-        Are you ready to transform your learning experience? Look no further than 1SHOT - LMS, the premier online learning platform designed to address the challenges of today's eLearning landscape for both educators and learners.
+        Are you ready to transform your learning experience? Look no further than 1SHOT  LMS, the premier online learning platform designed to address the challenges of today's eLearning landscape for both educators and learners.
           <br />
           <br />
           As the devs 1SHOT, we know firsthand the challenges
           that come with learning and growing in the programming industry.
-          That&apos;s why we created 1SHOT &ndash; to provide a space where education is accessible, engaging, and empowering for everyone.
+          That is why we created 1SHOT to provide a space where education is accessible, engaging, and empowering for everyone.
           <br />
           <br />
           At 1SHOT, we believe that quality education should never be out of reach. Our platform aims to democratize learning by offering high-quality educational experiences at competitive prices. We empower educators to share their expertise and creativity, ensuring that learners from all backgrounds can access the knowledge they seek.
@@ -26,10 +26,10 @@ const About = () => {
           1SHOT is committed to enhancing learner engagement. We offer interactive course materials and foster vibrant learning communities that encourage collaboration and peer-to-peer interaction. By creating a supportive environment, we help combat disengagement and promote a deeper understanding of course content.
           <br />
           <br />
-          At 1SHOT &ndash; LMS, we are more than just a platform; we are a community of passionate learners and educators. Whether you’re looking to enhance your skills or share your knowledge with others, 1SHOT is here to support you every step of the way.
+          At 1SHOT LMS, we are more than just a platform; we are a community of passionate learners and educators. Whether you are looking to enhance your skills or share your knowledge with others, 1SHOT is here to support you every step of the way.
           <br />
           <br />
-          So, what are you waiting for? Join the 1SHOT community today, and let’s reshape the future of education together. With our affordable courses and collaborative environment, the possibilities for your learning journey are limitless!
+          So, what are you waiting for? Join the 1SHOT community today, and lets reshape the future of education together. With our affordable courses and collaborative environment, the possibilities for your learning journey are limitless!
           <br />
           <br />
         </p>
